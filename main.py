91241a82766ae4279cdd46e2ecd46f3640089b3e
Julian Ozen <julian.ozen@me.com>
2013-07-23 16:54:38 -0500

    Revert "stuff on stuff"
    This reverts commit cc0e5cabe26914b9f0598515a760c302aa82832c.
    
    Conflicts:
    	main.py
    	main.pyc

--- main.py
+++ main.py
@@ -246,9 +246,12 @@ class NewItem(Handler):
 class LoginItem(Handler):
     def get(self, phone_error=""):
 <<<<<<< HEAD
+<<<<<<< HEAD
         
     
 =======
+=======
+>>>>>>> parent of cc0e5ca... stuff on stuff
         session = get_current_session()
 >>>>>>> parent of cc0e5ca... stuff on stuff
         self.render("login.html",phone_error = phone_error)
--- packages/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# the mock-0.3.1 dir contains testcase.py, testutils.py & mock.py
-path = os.path.dirname(__file__)
-sys.path.append(path)
-
