71f15dcb17a89f3f255baa7d17ddc13a6a4533cc
Julian Ozen <julian.ozen@me.com>
2013-07-28 16:48:10 -0500

    Reuploaded

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 
 import os
 import webapp2
@@ -27,6 +27,7 @@ import urllib2
 import string
 import packages
 import geopy
+import string
 #import appengine_utilities
 #import pycrypto
 from webapp2_extras import sessions
@@ -35,6 +36,8 @@ config = {}
 config['webapp2_extras.sessions'] = {
     'secret_key': 'c4047563e84e0dab8ddfbf78780542e59713396005fa232ec017e5a852eb7d687f28ff4b341f2802a4d597bd1357bdf14d4ba5c344c555b44cf55ed4f36d547c',
 }
+
+universalsalt = "dog"
 
 # from geopy import geocoders
 from google.appengine.ext import db
@@ -99,22 +102,27 @@ class Handler(webapp2.RequestHandler):
         return phone_error
 
     def check_value(self, value,kind):
-        expressions = {"email":"^[\S]+@[\S]+\.[\S]+$","password":"^.{3,20}$","username":"^[a-zA-Z0-9_-]{3,20}$", "phone":"^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$" }
-        if kind =="email": 
-            if value =="":
-                return True
+        expressions = {"password":"^.{3,20}$","username":"^[a-zA-Z0-9_-]{3,20}$", "phone":"^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$" }
         try:
             (re.findall(expressions[kind], value)[0] == value)
         except :
             return False
         return True
 
-    def confirm_verify_phone(self, phone):
-        name_error = ""
-        users = db.GqlQuery("SELECT * FROM People")
-        for user in users:
-            if str(db.PhoneNumber(phone)) == str(user.phone):
-                name_error = ""
+    def isPhoneinDatabase(self, phone):
+        name_error = "number already in database"
+        result = db.GqlQuery("SELECT * FROM People WHERE phone = %s" % phone)
+
+        # users = db.GqlQuery("SELECT * FROM People")
+        # for user in users:
+        #     if str(db.PhoneNumber(phone)) == str(user.phone):
+        #         name_error = "number already in database"
+        print("here")
+        print(result)
+        print(result.count(1))
+        if not result.count(1):
+            name_error = ""
+        print(name_error)
         return name_error
 
     def setcookie(self, encryption):
@@ -123,12 +131,14 @@ class Handler(webapp2.RequestHandler):
         self.response.headers.add_header('set-cookie', cookie)
 
     def validateUser(self):
-        curentlogin = self.request.cookies.get("phone", "error")
-        if curentlogin == "error":
-            self.redirect("/login")
-        if not self.confirm_phone(self.request.cookies.get("phone", "error")) == "":
-            self.redirect("/login")
-        #implement bad cookie
+        print('validation not setup')
+        # phone = self.session.get('phone')
+        # hashed = self.session.get('hash')
+        # phone = self.cleanPhone(phone)
+        # print(phone)
+        # user = db.GqlQuery("SELECT * FROM People WHERE phone = %s" % phone)[0] #gets what should be only user with number
+        # if not user.userhash == hashed:
+        #     self.redirect('/')
 
     def check_password(self, one,two):
         return one==two
@@ -160,10 +170,16 @@ class Handler(webapp2.RequestHandler):
         return str(hash)
     
     def cleanPhone(self, phone):
-        return int(re.sub("\D", "", phone))
+        all=string.maketrans('','')
+        nodigs=all.translate(all, string.digits)
+        return int(str(phone).translate(all, nodigs))
 
     def decryprt(self, hash):
         return self
+
+    def databaseIsEmpty(self, database):
+        empty = database.all()
+        return empty.count(1)
 
 
     def confirm_password(self, password, verify):
@@ -177,15 +193,6 @@ class Handler(webapp2.RequestHandler):
                 password_error = "These passwords are not the same!"
         return password_error
 
-
-    # def encode(key, string):
-    # encoded_chars = []
-    # for i in xrange(string):
-    #     key_c = key[i % len(key)]
-    #     encoded_c = chr(ord(string[i]) + ord(encoded_c) % 256)
-    #     encoded_chars.append(encoded_c)
-    # encoded_string = "".join(encoded_chars)
-    # return base64.urlsafe_b64encode(encoded_string)
     def getUser(self):
         return int(self.request.cookies.get("phone", "error"))
  
@@ -210,39 +217,52 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainPage(BaseHandler, Handler):
     def render_mainpage(self):
-
         Inventory = db.GqlQuery("SELECT * FROM Inventory")
         names = db.GqlQuery("SELECT * FROM People")
         catagory = db.GqlQuery("SELECT * FROM Catagory")
         self.render("main.html", items = Inventory, names = names)
 
     def get(self):
-        test_value = self.session.get('phone')
-        if test_value:
-            self.response.write('Session has this value: %r.' % test_value)
+        username = self.session.get('username')
+        phone = self.session.get('phone')
+        if username and phone:
+            self.response.write('welcome ' + username)
+        elif phone or username:
+            self.redirect('/logout')
         else:
-            self.session['test-value'] = 'Hello, session world!'
-            self.response.write('Session is empty.')
+            self.response.write('You should sign up or sign in.')
         self.render_mainpage()
+
+class Logout(BaseHandler, Handler):
+    def get(self):
+        self.session.clear()
+        self.redirect('/')
 
 class NewItem(BaseHandler, Handler):
     def render_newpost(self , ItemName ="", ItemDescription = "", ItemLocation = "", error = ""):
         self.render("newform.html", ItemName= ItemName, ItemDescription = ItemDescription, ItemLocation = ItemLocation, error = error)
 
     def get(self):
-        # self.validateUser()
-
+        self.validateUser()
         self.render_newpost()
 
     def post(self):
-        self.validateUser()
         ItemName = self.request.get("ItemName")
         ItemDescription = self.request.get("ItemDescription")
         ItemLocation = self.request.get("ItemLocation")
         canTransfer = self.request.get("canTransfer") == "True"
         isNeed = self.request.get("isNeed") == "True"
         catagory = self.request.get("catagory")
-        phone = self.getUser() #needs proper encryption
+
+        phone = self.session.get('phone')
+        phone = self.cleanPhone(phone)
+        newitem = Inventory(ItemName = ItemName, catagory = catagory, phone = phone, location = ItemLocation, isNeed = isNeed, isCompleted = False, ItemDescription = ItemDescription, canTransfer = canTransfer)
+
+
+
+
+
+
         matches = self.findMatches(isNeed) #needs to be more efficient
         matches = self.findClosets(matches) #needs to sort by location
         if len(matches) == 0:
@@ -273,19 +293,42 @@ class NewItem(BaseHandler, Handler):
 
 class LoginItem(BaseHandler, Handler):
     def get(self, phone_error=""):
-        self.render("login.html",phone_error = phone_error)
+        self.render("login.html",phone_error = "")
 
     def post(self, phone_error=""):
-        
+        phone_error=""
         phone = self.request.get('phone')
-        phone_error = self.confirm_phone(self.request.get('phone'))
-        phone = re.sub("\D", "", phone)
+        phone_error = self.confirm_phone(self.request.get('phone')) #confirms the number is valid type
+        phone = re.sub("\D", "", phone) #cleans numbers?
+        # user = People.all()
+        # if user.count(1):
+        #     phone_error = "empty"
         if phone_error == "":
-            phone_error = self.confirm_verify_phone(phone)
-            if phone_error == "":
-                self.session['phone'] = self.request.get('phone')
-                self.redirect("/")
-        self.render("login.html", phone_error = phone_error)
+            phone_error = self.isPhoneinDatabase(phone) #ensures phone is in database
+            if not phone_error == "":
+                phone = self.cleanPhone(phone) #cleans numbers
+                user = db.GqlQuery("SELECT * FROM People WHERE phone = %s" % phone)[0] #gets what should be only user with number
+                print(user.name, phone, self.request.get('password'))
+                testhash = self.make_hash(user.name, phone, self.request.get('password')) #generates hash with input number, password name drawn from database
+                print(testhash)
+                print(user.userhash) #hash on database
+                print(user.name)
+                if testhash == user.userhash: #compares local hash with hash in database
+                    phone_error = ""
+                    self.session['username'] = user.name 
+                    self.session['phone'] = phone
+                    self.session['hash'] = testhash
+                else:
+                    phone_error = "Password and number do not match"
+            # else:
+                # phone_error = "Number not in Database"
+        # else:
+            # phone_error = "Invalid Number"
+        print(phone_error)
+        if not phone_error:
+            self.redirect("/")
+        else:
+            self.render("login.html", phone_error = phone_error)
 
 class AddToDataBase(BaseHandler, Handler):
     def get(self, phone_error=""):
@@ -297,33 +340,37 @@ class AddToDataBase(BaseHandler, Handler):
 
 class SignUp(BaseHandler, Handler):
     def get(self,username="",name_error="", password = "", password_error = "", phone="",phone_error=""):
-       
-
         self.render("signup.html", username = username, password = password, password_error = password_error, name_error = name_error, phone = phone, phone_error = phone_error)
 
     def post(self, username="",name_error="",phone="",password = "", password_error = "", phone_error=""): 
-        phone = self.request.get('phone')
-        phone_error = self.confirm_phone(self.request.get(phone))
-        password_error = self.confirm_password(self.request.get('password'), self.request.get('verify'))
-        phone = self.cleanPhone(phone)
-        phone_error = self.confirm_phone(self.request.get('phone')) #####################
-        if phone_error == "":
-            name_error = self.confirm_verify_username(self.request.get('username'),phone) #################
+        phone = self.cleanPhone(self.request.get('phone')) #stores phone
+        phone_error = self.confirm_phone(self.request.get('phone')) #checks if number is valid
+        password_error = self.confirm_password(self.request.get('password'), self.request.get('verify'))#checks if passwords match
+        phone_error = self.isPhoneinDatabase(phone)
         if  name_error =="" and phone_error=="" and password_error == "":
-            self.success(self.request.get('username'), phone, password,session)
+            self.success(self.request.get('username'), phone, self.request.get('password'))
         else:
-            self.render("signup.html", username = username, name_error = name_error, phone = phone, phone_error = phone_error)
+            self.render("signup.html", username = self.request.get('username'), name_error = name_error, phone = self.request.get('phone'), phone_error = phone_error)
 
-    def success(self, username, phone,password,session):
-        userhash = self.make_hash(username, phone,password)
-        session['user'] = userhash
-        session['phone'] = phone
-        newuser = People(name = username, password = password, userhash= userhash, phone = self.cleanPhone(phone))
+    def success(self, username, phone,password):
+        phone = self.cleanPhone(phone)
+        print(username, phone, password)
+        userhash = self.make_hash(username, phone, password)
+        self.session['username'] = username
+        self.session['phone'] = phone
+        self.session['hash'] = userhash
+        newuser = People(name = username, password = password, userhash= userhash, phone = phone)
         newuser.put()
         self.redirect("/")
 
 
-app = webapp2.WSGIApplication([('/', MainPage),('/new/?', NewItem),('/login/?', LoginItem),('/add/?', AddToDataBase),('/signup/?', SignUp)], config=config, debug=True)
+app = webapp2.WSGIApplication([('/', MainPage),
+    ('/new/?', NewItem),
+    ('/login/?', LoginItem),
+    ('/add/?', AddToDataBase),
+    ('/signup/?', SignUp),
+    ('/logout', Logout)],
+     config=config, debug=True)
 
 def main():
     app.run()
